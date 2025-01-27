a6b87cc19d15d4205b1a2ffb66aec0d83e8378e8
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-01-27 18:12:13 -0300

    almost finished

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const d = document;
 const shareBtn = d.querySelector('.share-icon-container');
-const userContainer = d.querySelector('.user-container');
-const socialContainer = d.querySelector('.social-container')
 const socialIconsContainer = d.querySelector('.social-icons-container')
 const shareIconBlue = d.querySelector('.share-icon-blue');
 const shareIconWhite = d.querySelector('.share-icon-white');
